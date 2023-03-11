76257162f27d5221a5efd28f5d5cec5fc9c2ea31
fliderman01 <fliderman01@gmail.com>
2023-03-11 14:16:53 +0400

    added Meals.tsx

---
+++ src/components/Meals.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Meal from "./Meal";
+import { Food } from "../types";
+
+type MealsProps = {
+  searchQuery: string;
+};
+
+const Meals: React.FC<MealsProps> = ({ searchQuery }) => {
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    const url = searchQuery ? `/api/search?query=${searchQuery}` : "/api/foods";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFoods(data))
+      .catch((error) => console.log(error));
+  }, [searchQuery]);
+
+  return (
+    <div>
+      {foods.map((food) => (
+        <Meal key={food.id} food={food} />
+      ))}
+    </div>
+  );
+};
+
+export default Meals;
